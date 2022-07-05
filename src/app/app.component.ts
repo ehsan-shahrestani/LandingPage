3b91b401029847af152224da968066f9afb8f3a3
ehsan <ehsan.shahrestani1@gmail.com>
2022-07-05 21:14:02 +0430

    add tailwind and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+
+}
+
+export const ROUTES: RouteInfo[] = [
+  { path: '', title: '.Category' },
+  { path: '', title: '.Home' },
+  { path: '', title: '.Contact us ' },
+  { path: '', title: '.Faq' },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'landingpage';
+  menuItems!: any[]
+  ngOnInit(): void {
+    this.menuItems = ROUTES
+  }
 }
